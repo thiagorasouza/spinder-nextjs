4baf76cde5f22d205a5917f197570889efa2c0aa
Thiago Souza <thiagorasouza@gmail.com>
2022-06-02 02:28:56 -0300

    fix crossorigin attribute on font link element

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,11 @@ export default function Document() {
       <Head>
         <link rel="icon" href="favicon.ico?v=1" type="image/x-icon" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap"
           rel="stylesheet"
