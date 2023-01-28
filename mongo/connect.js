ef1615f7e3c9de84859e89e4035c4f7f09efdfc4
Thiago Souza <thiagorasouza@gmail.com>
2023-01-28 07:23:51 -0500

    fix: set only one connection listener on mongoose

--- mongo/connect.js
+++ mongo/connect.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 
+mongoose.connection.on("error", (error) => {
+  throw new Error("There was a problem with the database connection");
+});
+
 async function connect() {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    mongoose.connection.on("error", (error) => {
-      throw new Error("There was a problem with the database connection");
-    });
   } catch (error) {
     throw new Error("Could not establish a connection to the database");
   }
