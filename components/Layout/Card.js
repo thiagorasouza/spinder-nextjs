e22a3d1d0d54a7e25a53d4c1fc341cfb03d27dc3
Thiago Souza <thiagorasouza@gmail.com>
2023-01-29 11:48:27 -0500

    refactor: replace css modules with bootstrap in Card component

--- components/Layout/Card.js
+++ components/Layout/Card.js
@@ -1,9 +1,15 @@
-import styles from "./Card.module.css";
+import classNames from "classnames";
 
 function Card(props) {
   return (
-    <div className={styles.container}>
-      <div className={styles.card + " " + props.className}>
+    <div className="m-auto position-absolute top-0 start-0 bottom-0 end-0 d-flex flex-column justify-content-center align-items-center py-2">
+      <div
+        className={classNames(
+          "w-100 py-3 px-4 bg-white rounded-1 shadow-sm",
+          props.className
+        )}
+        style={{ maxWidth: "20rem" }}
+      >
         {props.children}
       </div>
     </div>
